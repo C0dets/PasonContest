1f507ebd814fd9cbf11ab13503acee41755e4b1d
LindsayL <revengeofthefrogs@hotmail.com>
2016-01-07 23:56:07 -0700

    debugging

--- communication.py
+++ communication.py
@@ -73,8 +73,11 @@ class Comm:
 
     def sendCommand(self, command):
         command["client_token"] = self.clientToken
+        print 'sending'
         self.commandChannel.send(json.dumps(command))
+        print 'sent'
         message = json.loads(self.commandChannel.recv())
+        print 'recieved'
         if (message["comm_type"] == "ErrorResp"):
             print command
             print message
